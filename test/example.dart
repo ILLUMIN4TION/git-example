998ae2db22a0ae590a579c9ea33458a8479379ec
ILLUMIN4TION <skawogus3358@gmail.com>
2025-03-20 11:27:47 +0900

    myFirstGit

--- test/example.dart
+++ test/example.dart
@@ -1,14 +1,27 @@
 void main(){
 
-  try{
-    //var index = 0;
-    throw Exception('error');
-    //index = '1';
-    print('Try');
-  } catch(e) {
-    print('catch');
-  }finally{
-    print('finally');
+  final list = [2, 1, 3, 3, 4];
+  for(var i = 0; i<list.length; i++ ) {
+    print('1번째: ${list[i]}');
+
   }
 
+
+
+  //2.
+  list.forEach((item) {
+    print('2번쨰:! $item');
+  });
+  
+
+
+  //3. //리스트의 길ㅇ
+  List.generate(list.length, (i){
+    final item = list[i];
+
+    print('3번째: $item');
+  });
+  
 }
+
+
